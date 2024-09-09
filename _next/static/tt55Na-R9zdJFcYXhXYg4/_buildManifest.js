1f2b4caeca6c3358a75acec4c31d3389fc4abbca
abedgharra123 <abedd123gharra@gmail.com>
2024-09-09 17:18:25 +0300

    Updates

--- _next/static/tt55Na-R9zdJFcYXhXYg4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0c428ae2-bcf0ba6d4f231caf.js","static/chunks/d7eeaac4-603f703880ee4c93.js","static/chunks/pages/index-30407ccd840470c9.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
